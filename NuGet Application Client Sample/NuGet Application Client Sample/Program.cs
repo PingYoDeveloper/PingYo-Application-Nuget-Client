684f2974e2f0e5d145d5d87ee1841b572f2c45fa
Developer <developer@pingyo.com>
2015-01-12 01:39:57 +0000

    Adds notes to explain how the process works

--- NuGet Application Client Sample/NuGet Application Client Sample/Program.cs
+++ NuGet Application Client Sample/NuGet Application Client Sample/Program.cs
@@ -8,22 +8,45 @@ namespace NuGet_Application_Client_Sample
     {
         static void Main(string[] args)
         {
+            // 1 -- set test mode until your ready to go live
             var isTestApplication = true;
+
+            // 2 -- set the entry point for the ping tree system you are targeting
             var submissionUrl = new Uri("https://leads.pingyo.com");
 
             var client = new PingTreeClient(submissionUrl, isTestApplication);
+
+            // optional, set call-back to monitor status
             client.OnStatusChanged += client_OnStatusChanged;
 
+            // 3 -- Set your affiliate ID to ensure you get credit for your leads
             var affiliateId = "TEST";
+
+            // 4 -- Create your request
             var request = CreateRequest();
+
+            // 5 -- Set your desired timeout. More time means more lenders
             var timeout = 12000;
 
-            var response = client.SubmitApplication(affiliateId, request, timeout);
+            PingTreeApplicationResponse response;
+            try
+            {
+                // 6 -- Submit your application for processing and wait for a response
+                // Test submissions will create random outcomes
+                response = client.SubmitApplication(affiliateId, request, timeout);
 
-            Console.WriteLine("CorrelationID: " + response.CorrelationId);
-            Console.WriteLine("Final Status: " + response.Status);
-            Console.WriteLine("Estimated Commission: " + response.EstimatedCommission);
-            Console.WriteLine("Redirect URL: " + response.RedirectionUrl);
+                Console.WriteLine("CorrelationID: " + response.CorrelationId);
+                Console.WriteLine("Final Status: " + response.Status);
+                Console.WriteLine("Estimated Commission: " + response.EstimatedCommission);
+
+                // IMPORTANT - If the redirect url is not null, a redirect of the applicant is expected or commission may be withheld
+
+                Console.WriteLine("Redirect URL: " + response.RedirectionUrl);
+            }
+            catch (PingTreeAffiliateException ex)
+            {
+                Console.WriteLine("Application Rejected As Invalid: " + ex.FaultDetails.Message);                
+            }
 
             Console.ReadLine();
         }
@@ -31,10 +54,13 @@ namespace NuGet_Application_Client_Sample
         static void client_OnStatusChanged(object sender, StatusChangedEventArgs e)
         {
             Console.WriteLine("Percentage Complete: " + e.Status.PercentageComplete);
+            // Note: Test submissions will only produce zero percentage updates
         }
 
         private static PingTreeApplicationRequest CreateRequest()
         {
+            // Refer to the online integration Id for details on individual fields
+
             var request = new PingTreeApplicationRequest
             {
                 Application = CreateApplication(),
@@ -48,6 +74,8 @@ namespace NuGet_Application_Client_Sample
 
         private static ProxyApplication CreateApplication()
         {
+            // Refer to the online integration Id for details on individual fields
+
             var application = new ProxyApplication
             {
                 AddressCity = "Test",
@@ -109,6 +137,8 @@ namespace NuGet_Application_Client_Sample
 
         private static SourceDetails CreateSourceDetails()
         {
+            // Refer to the online integration Id for details on individual fields
+
             var details = new SourceDetails
             {
                 Address = "123.456.789.345",
